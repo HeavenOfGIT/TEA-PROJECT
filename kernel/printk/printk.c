d38ddeded2300d14c46a083b2c837986d764249c
Julian Liu <wlootlxt123@gmail.com>
2019-10-02 07:41:11 +0800

    printk: silence healthd and batteryd logs
    * <3>batteryd, <4>healthd
    
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -649,6 +649,10 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 	 */
 	line = buf;
 	if (line[0] == '<') {
+		if (memcmp(line+3, "batteryd", sizeof("batteryd")-1) == 0 ||
+			   memcmp(line+3, "healthd", sizeof("healthd")-1) == 0)
+			goto ignore;
+		{
 		char *endp = NULL;
 		unsigned int u;
 
@@ -668,10 +672,11 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 				}
 			}
 		}
+		}
 	}
 
 	printk_emit(facility, level, NULL, 0, "%s", line);
-free:
+ignore:
 	kfree(buf);
 	return ret;
 }
